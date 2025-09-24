447b20bdfedb2f08e89d0b6c539f774f15e9387e
Patryk Prentki <patryk.prentki90@gmail.com>
2025-09-24 23:58:32 +0200

    Modify feeder

--- src/test/java/example/feeders/MyFeeders.java
+++ src/test/java/example/feeders/MyFeeders.java
@@ -7,12 +7,8 @@ import static io.gatling.javaapi.core.CoreDsl.jsonFile;
 public class MyFeeders {
 
 
-    public static FeederBuilder<Object> feederBuilderSingleObject() {
-        return  jsonFile("D:\\GATLING\\gatling-maven-plugin-demo-java-main\\src\\test\\java\\example\\utils\\singleVideoGame.json").queue();
-    }
-
     public static FeederBuilder<Object> feederBuilderManyObjects() {
-        return jsonFile("utils/VideoGames.json").circular();
+        return jsonFile("D:\\GATLING\\gatling-maven-plugin-demo-java-main\\src\\test\\java\\example\\utils\\VideoGames.json").random();
     }
 
 }
