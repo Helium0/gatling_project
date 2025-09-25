58ceaca5a9c7c9d3f2e2a703584373f2173972cb
Patryk Prentki <patryk.prentki90@gmail.com>
2025-09-25 23:29:56 +0200

    Changed endpoints on different API

--- src/test/java/example/requests/APIEndpoints.java
+++ src/test/java/example/requests/APIEndpoints.java
@@ -1,37 +1,58 @@
 package example.requests;
 
+import example.utils.ReadingProperties;
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 
 
-import io.gatling.javaapi.core.*;
-import io.gatling.javaapi.http.*;
-
 public class APIEndpoints extends Simulation {
 
-    public static final HttpProtocolBuilder httpProtocol = http.baseUrl("https://reqres.in/api")
-            .header("x-api-key","reqres-free-v1");
+    public static final HttpProtocolBuilder httpProtocol;
 
-    public static  HttpRequestActionBuilder getAllUsers() {
-        return http("Get All Users")
-                .get("/users")
+    static {
+        try {
+            httpProtocol = http.baseUrl(ReadingProperties.getFileValue("baseUrl"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static  HttpRequestActionBuilder getAllVideoGames() {
+        return http("Get All Video Games")
+                .get("/videogame")
+                .check(status().is(200))
+                .check(jsonPath("$[*].id").findAll().transform(id -> id.stream().map(ids -> Integer.parseInt(ids.toString())).collect(Collectors.toList())).saveAs("allGamesId"))
+                .check(jsonPath("$[*].name").findAll().saveAs("gameTitle"));
+    }
+
+    public static HttpRequestActionBuilder getSingleVideoGame() {
+        return http("Get Single Video Game")
+                .get("/videogame/#{gameId}")
+                .check(bodyString().saveAs("fullBody"))
                 .check(status().is(200));
     }
 
-    public static HttpRequestActionBuilder getSingleUser() {   //get hard coded user
-        return http("Get Single User")
-                .get("/users/2")
-                .check(status().is(200));
-    }
-
-    public static HttpRequestActionBuilder createUser() {
-        return http("Create User")
-                .post("/users")
+    public static HttpRequestActionBuilder createVideoGame() {
+        return http("Create Single Video Game")
+                .post("/videogame")
+                .headers(TokenAPI.getAuthorizationHeaders())
                 .asJson()
-                .body(RawFileBody("utils/user.json"))
-                .check(status().is(201));
+                .body(ElFileBody("D:\\GATLING\\gatling-maven-plugin-demo-java-main\\src\\test\\java\\example\\utils\\VideoGameTemplate.json"))
+                .check(status().is(200));
+
     }
 
-
-
+    public static HttpRequestActionBuilder deleteVideoGame() {
+        return http("Delete Video Game")
+                .delete("/videogame/#{gameId}")
+                .headers(TokenAPI.getAuthorizationHeaders())
+                .check(status().is(200))
+                .check(bodyString().is("Video game deleted"))
+                .check(bodyString().saveAs("body"));
+    }
 }
