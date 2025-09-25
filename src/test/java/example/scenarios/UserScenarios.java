835f10a932b2f3ef7cb03c7374029f706819348c
Patryk Prentki <patryk.prentki90@gmail.com>
2025-09-25 23:29:22 +0200

    Refactored class and methods

--- src/test/java/example/scenarios/UserScenarios.java
+++ src/test/java/example/scenarios/UserScenarios.java
@@ -1,6 +1,9 @@
 package example.scenarios;
 
+import example.feeders.MyFeeders;
 import example.requests.APIEndpoints;
+import example.requests.TokenAPI;
+import example.utils.Helper;
 import io.gatling.javaapi.core.ScenarioBuilder;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
@@ -8,16 +11,61 @@ import static io.gatling.javaapi.core.CoreDsl.*;
 
 public class UserScenarios {
 
-    public static ScenarioBuilder getAllUsersScenario = scenario("Get All Users Scenario")
-        .exec(APIEndpoints.getAllUsers());
+    public static ScenarioBuilder getAllVideoGamesScenario = scenario("Get All Video Games Scenario")
+            .exec(APIEndpoints.getAllVideoGames());
 
-    public static ScenarioBuilder getSingleUserScenario = scenario("Get Single User Scenario")
-        .exec(APIEndpoints.getSingleUser());
+    public static ScenarioBuilder getSingleVideoGameScenario = scenario("Get Single Video Game Scenario")
+            .exec(APIEndpoints.getAllVideoGames())
+            .pace(2)
+            .exec(session -> {
+                return Helper.videoGameId(session,"allGamesId", "gameId");
+            })
+            .exec(APIEndpoints.getSingleVideoGame())
+            .exec(session -> {
+                System.out.println("GAME ID: " + session.getString("gameId"));
+                System.out.println("GAME: " + session.getString("fullBody"));
+                return session;
+            });
 
-    public static ScenarioBuilder createUser = scenario("Create User")
-        .exec(APIEndpoints.createUser());
 
-    public static ScenarioBuilder createUserFeeder = scenario("Multiple Users")
-            .exec(feed(jsonFile("utils/user.json").circular()));
+    public static ScenarioBuilder createSingleVideoGameScenario = scenario("Load Video Game And Create")
+            .exec(feed(MyFeeders.feederBuilderManyObjects()))
+            .pace(2)
+            .exec(TokenAPI.getToken())
+            .pace(2)
+            .exec(APIEndpoints.createVideoGame());
 
+    public static ScenarioBuilder deleteSingleVideoGameScenario = scenario("Delete Random Video Game")
+            .exec(APIEndpoints.getAllVideoGames())
+            .pace(2)
+            .exec(session -> {
+                return Helper.videoGameId(session,"allGamesId", "gameId");
+
+            })
+            .exec(TokenAPI.getToken())
+            .exec(APIEndpoints.deleteVideoGame())
+            .exec(session -> {
+                System.out.println("BODY: " + session.getString("body"));
+                return session;
+            });
+
+    public static ScenarioBuilder CRUDScenario = scenario("CRUD")
+            .exec(feed(MyFeeders.feederBuilderManyObjects()))
+            .pace(2)
+            .exec(TokenAPI.getToken())
+            .pace(2)
+            .exec(APIEndpoints.createVideoGame())
+            .exec( session -> {
+                System.out.println("BODY: " + session.getString("fullBody"));
+                return session;
+            })
+            .pace(2)
+            .exec(APIEndpoints.getSingleVideoGame())
+            .exec( session -> {
+                System.out.println("GAME ID: " + session.getString("gameId"));
+//                System.out.println("GAME : " + session.getString("fullBody"));
+                return session;
+            })
+            .pace(2)
+            .exec(APIEndpoints.deleteVideoGame());
 }
