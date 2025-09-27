01f625bc8da7a309470a8738c261dad0f53c4009
Patryk Prentki <patryk.prentki90@gmail.com>
2025-09-28 00:59:38 +0200

    Refactored scenarios

--- src/test/java/example/scenarios/UserScenarios.java
+++ src/test/java/example/scenarios/UserScenarios.java
@@ -4,68 +4,63 @@ import example.feeders.MyFeeders;
 import example.requests.APIEndpoints;
 import example.requests.TokenAPI;
 import example.utils.Helper;
+import io.gatling.javaapi.core.ChainBuilder;
 import io.gatling.javaapi.core.ScenarioBuilder;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.internal.HttpCheckBuilders.*;
 
 
 public class UserScenarios {
 
-    public static ScenarioBuilder getAllVideoGamesScenario = scenario("Get All Video Games Scenario")
-            .exec(APIEndpoints.getAllVideoGames());
+    public static ChainBuilder getAllVideoGames =
+            exec(APIEndpoints.getAllVideoGamesRequest());
 
     public static ScenarioBuilder getSingleVideoGameScenario = scenario("Get Single Video Game Scenario")
-            .exec(APIEndpoints.getAllVideoGames())
+            .exec(getAllVideoGames)
             .pace(2)
             .exec(session -> {
                 return Helper.videoGameId(session,"allGamesId", "gameId");
             })
-            .exec(APIEndpoints.getSingleVideoGame())
-            .exec(session -> {
-                System.out.println("GAME ID: " + session.getString("gameId"));
-                System.out.println("GAME: " + session.getString("fullBody"));
-                return session;
-            });
+            .exec(APIEndpoints.getSingleVideoGameRequest());
 
 
     public static ScenarioBuilder createSingleVideoGameScenario = scenario("Load Video Game And Create")
-            .exec(feed(MyFeeders.feederBuilderManyObjects()))
+            .exec(feed(MyFeeders.feederBuilderForCreateVideoGame()))
             .pace(2)
+            .exitHereIfFailed()
             .exec(TokenAPI.getToken())
-            .pace(2)
-            .exec(APIEndpoints.createVideoGame());
+            .tryMax(3)
+            .on(exec(APIEndpoints.createVideoGameRequest()
+                    .check(status().in(200,201),
+                            responseTimeInMillis().lt(1000)))
+            .exec(APIEndpoints.createVideoGameRequest()))
+            .doIf(session ->
+                    session.isFailed()).then(exec(session -> {
+                System.out.println("Failed create game after 3 attempts");
+                return session;
+            }));
 
     public static ScenarioBuilder deleteSingleVideoGameScenario = scenario("Delete Random Video Game")
-            .exec(APIEndpoints.getAllVideoGames())
+            .exec(getAllVideoGames)
             .pace(2)
             .exec(session -> {
                 return Helper.videoGameId(session,"allGamesId", "gameId");
 
             })
             .exec(TokenAPI.getToken())
-            .exec(APIEndpoints.deleteVideoGame())
-            .exec(session -> {
-                System.out.println("BODY: " + session.getString("body"));
-                return session;
-            });
+            .exec(APIEndpoints.deleteVideoGameRequest());
 
-    public static ScenarioBuilder CRUDScenario = scenario("CRUD")
-            .exec(feed(MyFeeders.feederBuilderManyObjects()))
+    public static ScenarioBuilder updateSingleVideoGameScenario = scenario("Update Random Video Game")
+            .exec(feed(MyFeeders.feederBuilderForUpdateVideoGame()))
             .pace(2)
+            .exitHereIfFailed()
+            .exec(getAllVideoGames)
+            .pace(2)
+            .exec(session -> {
+                return Helper.videoGameId(session,"allGamesId", "gameId");
+            })
             .exec(TokenAPI.getToken())
             .pace(2)
-            .exec(APIEndpoints.createVideoGame())
-            .exec( session -> {
-                System.out.println("BODY: " + session.getString("fullBody"));
-                return session;
-            })
-            .pace(2)
-            .exec(APIEndpoints.getSingleVideoGame())
-            .exec( session -> {
-                System.out.println("GAME ID: " + session.getString("gameId"));
-//                System.out.println("GAME : " + session.getString("fullBody"));
-                return session;
-            })
-            .pace(2)
-            .exec(APIEndpoints.deleteVideoGame());
+            .exec(APIEndpoints.updateVideoGameRequest());
 }
