4456e208d79d87f4b271fd92becff12b5d21153f
Patryk Prentki <patryk.prentki90@gmail.com>
2025-09-28 00:57:59 +0200

    Edited scenario to be more advanced

--- src/test/java/example/simulations/CreateSingleVideoGame.java
+++ src/test/java/example/simulations/CreateSingleVideoGame.java
@@ -2,17 +2,21 @@ package example.simulations;
 
 import example.requests.APIEndpoints;
 import example.scenarios.UserScenarios;
+import example.utils.Config;
 import io.gatling.javaapi.core.Simulation;
 
-import static io.gatling.javaapi.core.CoreDsl.atOnceUsers;
+import static io.gatling.javaapi.core.CoreDsl.*;
 
 public class CreateSingleVideoGame extends Simulation {
 
 
     {
         setUp(UserScenarios.createSingleVideoGameScenario.
-                injectOpen(atOnceUsers(1)))
-                .protocols(APIEndpoints.httpProtocol);
+                injectOpen(rampUsers(Config.getUsers()).during(Config.getDuration()),
+                        constantUsersPerSec(5).during(Config.getDuration()),
+                        rampUsersPerSec(1).to(10).during(Config.getDuration()),
+                        constantUsersPerSec(2).during(Config.getDuration()),
+                        atOnceUsers(Config.getAtOnceUsers()))
+                .protocols(APIEndpoints.httpProtocol));
     }
-
 }
