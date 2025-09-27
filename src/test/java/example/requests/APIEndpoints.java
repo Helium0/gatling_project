1c79c89cead2558940806f67dbbf6a6fd9b0c7c2
Patryk Prentki <patryk.prentki90@gmail.com>
2025-09-27 23:10:23 +0200

    refactored request names and provided relative path to the body

--- src/test/java/example/requests/APIEndpoints.java
+++ src/test/java/example/requests/APIEndpoints.java
@@ -18,11 +18,11 @@ public class APIEndpoints extends Simulation {
         try {
             httpProtocol = http.baseUrl(ReadingProperties.getFileValue("baseUrl"));
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to load base URL", e);
         }
     }
 
-    public static  HttpRequestActionBuilder getAllVideoGames() {
+    public static  HttpRequestActionBuilder getAllVideoGamesRequest() {
         return http("Get All Video Games")
                 .get("/videogame")
                 .check(status().is(200))
@@ -30,29 +30,38 @@ public class APIEndpoints extends Simulation {
                 .check(jsonPath("$[*].name").findAll().saveAs("gameTitle"));
     }
 
-    public static HttpRequestActionBuilder getSingleVideoGame() {
+    public static HttpRequestActionBuilder getSingleVideoGameRequest() {
         return http("Get Single Video Game")
                 .get("/videogame/#{gameId}")
                 .check(bodyString().saveAs("fullBody"))
                 .check(status().is(200));
     }
 
-    public static HttpRequestActionBuilder createVideoGame() {
+    public static HttpRequestActionBuilder createVideoGameRequest() {
         return http("Create Single Video Game")
                 .post("/videogame")
                 .headers(TokenAPI.getAuthorizationHeaders())
                 .asJson()
-                .body(ElFileBody("D:\\GATLING\\gatling-maven-plugin-demo-java-main\\src\\test\\java\\example\\utils\\VideoGameTemplate.json"))
+                .body(ElFileBody("bodies/VideoGameTemplate.json"))
                 .check(status().is(200));
 
     }
 
-    public static HttpRequestActionBuilder deleteVideoGame() {
+    public static HttpRequestActionBuilder deleteVideoGameRequest() {
         return http("Delete Video Game")
                 .delete("/videogame/#{gameId}")
                 .headers(TokenAPI.getAuthorizationHeaders())
                 .check(status().is(200))
                 .check(bodyString().is("Video game deleted"))
                 .check(bodyString().saveAs("body"));
+    }
+
+    public static HttpRequestActionBuilder updateVideoGameRequest() {
+        return http("Update Video Game")
+                .put("/videogame/#{gameId}")
+                .headers(TokenAPI.getAuthorizationHeaders())
+                .body(ElFileBody("bodies/VideoGameTemplate.json"))
+                .check(bodyString().saveAs("updateVideo"))
+                .check(status().is(200));
     }
 }
